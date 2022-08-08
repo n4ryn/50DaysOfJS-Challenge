ea4ada0794862b5507330a804803d1c3475c5180
Vinay Kumar <104409366+n4ryn@users.noreply.github.com>
2022-08-09 01:25:04 +0530

    Delete Day2.js

--- Day2.js
+++
@@ -1,8 +0,0 @@
-const str = "Peter Parker is Spiderman";
-
-function reverseAString(str) {
-  // Write your solution here
-  return str.split("").reverse().join("");
-}
-
-console.log(`Reversed string is: ${reverseAString(str)}`);
