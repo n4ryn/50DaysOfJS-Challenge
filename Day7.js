89ac78dc8433445c3f1bfe5fd5ec686985e0057f
n4ryn <vinaykumar.cs19@aryacollege.in>
2022-08-12 23:45:52 +0530

    Day7 - Create a regular expression to validate if the given input is valid Indian mobile number or not

---
+++ Day7.js
@@ -0,0 +1,10 @@
+const number = "+919876543210";
+
+function validateMobile(number) {
+  // write your solution here
+
+  const regexMobile = /^(\+91|0)?\s?\d{10}$/;
+  return regexMobile.test(number);
+}
+
+console.log(`is a valid Indian mobile number: ${validateMobile(number)}`);
